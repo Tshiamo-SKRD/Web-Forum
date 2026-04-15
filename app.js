df5b3ef0f4827f9cd17f36a68dd8a8f91073efaa
Tshiamo-SKRD <141047949+Tshiamo-SKRD@users.noreply.github.com>
2026-04-15 22:35:50 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+// app.js
+
+const express = require("express");
+const path = require("path");
+const pageRoutes = require("./routes/pageRoutes");
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/images", express.static('images'));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use("/", pageRoutes);
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
